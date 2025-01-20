48811e24dbcef4f46332c28992543bed449ca403
Somto <imaginbright@icloud.com>
2025-01-20 18:10:10 +0100

    initial commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,44 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./Videocarousel";
+import { animatewithGsap } from "../utils/animations";
+
+const Highlights = () => {
+  useGSAP(() => {
+    animatewithGsap("#title", { opacity: 1, y: 0 })
+    animatewithGsap('.link', {opacity: 1, y: 0, duration: 1, stagger: 0.25})
+  }, []);
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="Play image" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="next" className="ml-2" />
+            </p>
+          </div>
+
+        </div>
+
+        <VideoCarousel />
+
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
